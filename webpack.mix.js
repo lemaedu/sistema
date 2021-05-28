b28ad828be05e971cb5fd25d8b56cc5721446446
lemaedu <lemaedu@gmail.com>
2021-05-28 18:01:13 -0500

    Proyecto inicial

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,23 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.styles([
+    'resources/css/font-awesome.min.css',
+    'resources/css/simple-line-icons.min.css',
+    //Main styles for this application
+    'resources/css/style.css',
+
+], 'public/css/plantilla.css')
+
+.scripts([
+    // <!-- Bootstrap and necessary plugins -->
+    'resources/js/jquery.min.js',
+    'resources/js/popper.min.js',
+    'resources/js/bootstrap.min.js',
+    'resources/js/pace.min.js',
+    // <!-- Plugins and scripts required by all views -->
+    'resources/js/Chart.min.js',
+    //<!-- GenesisUI main scripts -->
+    'resources/js/template.js'
+
+], 'public/js/plantilla.js');
